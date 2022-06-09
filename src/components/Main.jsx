29a591f6ffeb59616a7eae95d0ef369f7c08850b
jkunciene <j.kunciene@gmail.com>
2022-06-09 15:14:29 +0300

    iki useEffect

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Films from './Films'
 import Search from './Search'
 
 const Main = () => {
-    //tevinis komponentas, kuriame gausiu duomenis is api
-    //kuris tures du child components: search, card
+
+  //pirmas state saugoja paieskos zodi
+  const [searchValue, setSearch] = useState('');
+  //antras state saugoja gautus duomenis is api
+  const [films, SetFilms] = useState('');
+
+//funkcija, kuri uzsetina paieskos zodi i Main state
+const setSearchValue = (val) =>{
+  //state update method
+  setSearch(val);
+}
+
+
+//useEfefect atsakingas uz metodo getFilms iskvietima
+//tikrina, ar pasikeite state reiksme -> searchValue
+//jei pasikeite runina viduje aprasyta metoda -> getFilms
+
+  useEffect(()=>{
+    getFilms(searchValue);
+  }, [searchValue])
 
   return (
     <div>
         <h1>Movies App</h1>
-        <Search/>
+        <Search setSearchValue={setSearchValue}/>
         <Films/>
     </div>
   )
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Search = () => {
+const Search = ({setSearchValue}) => {
+    const [movieTitle, setMovieTitle] = useState('');
+
+    //sitoje vietoje apsirasyti funkcija
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(movieTitle===""){
+            alert("iveskite filmo pavadinima...")
+        } else{
+            setSearchValue(movieTitle);
+            setMovieTitle('');
+        }
+        
+    }
   return (
     <div className='container'>
             <h2 className='row justify-content-center'>Find your movie</h2>
-            <form className='row  justify-content-center'>
+            <form className='row  justify-content-center' onSubmit={handleSubmit}>
                 <div className="form-group">
                     <input
                         type="text"
                         name="title"
                         className="form-control"
-                        placeholder="Write movie title"/>
+                        placeholder="Write movie title"
+                        onChange={(e)=>setMovieTitle(e.target.value)}
+                        value={movieTitle}/>
                 </div>
             </form>
         </div>
